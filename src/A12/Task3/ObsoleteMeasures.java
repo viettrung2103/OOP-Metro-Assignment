9590c515c7baf3220837c1ed723f3651d8f7a0b4
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-10-21 17:49:02 +0300

    finish a1.2, task3

---
+++ src/A12/Task3/ObsoleteMeasures.java
@@ -0,0 +1,29 @@
+package A12.Task3;
+
+import java.util.Scanner;
+
+public class ObsoleteMeasures {
+    public static void main(String[] args){
+        final double LUOTI = 13.28;
+        final double NAULA = 32 * LUOTI;
+        final double LEVISKA = 20 * NAULA;
+
+        int  leiviska, naula;
+        double luoti, input, remain;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Weight (g): ");
+        input = Double.parseDouble(scanner.nextLine());
+        remain = input;
+        leiviska = (int)(remain / LEVISKA);
+        remain = remain -  (leiviska * LEVISKA);
+        naula = (int)(remain/NAULA);
+        remain = remain - (naula * NAULA);
+        luoti = remain/LUOTI;
+
+        System.out.printf("%d grams is %d leiviska, %d naula, %.2f luoti", (int)input,leiviska,naula,luoti);
+
+
+    }
+}
