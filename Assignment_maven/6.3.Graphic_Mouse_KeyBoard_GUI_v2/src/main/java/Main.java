f808b09c05c0fbb1e7129c6a924f7d9a392b833c
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-11-28 16:47:30 +0200

    Remove unneed project, add UI for 6.4

--- Assignment_maven/6.3.Graphic_Mouse_KeyBoard_GUI_v2/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import view.PetUI;
-
-public class Main {
-    public static void main(String[] args) {
-        PetUI.launch(PetUI.class);
-    }
-}
--- Assignment_maven/6.3.Graphics_Mouse_Keyboard_GUI_v3/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import view.PetUI;
-
-public class Main {
-    public static void main(String[] agrs) {
-        PetUI.launch(PetUI.class);
-    }
-}
